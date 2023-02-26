193aebdbc4adba4cd238af9712cc66c3e7fea655
arix3767 <103655172+arix3767@users.noreply.github.com>
2023-02-26 15:20:47 +0100

    Initial commit

---
+++ Spring2/src/main/java/com/example/demo/MovieRentalClasses.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalClasses {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalClasses.class, args);
+	}
+
+}
